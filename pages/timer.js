e76936d5388eb3a0de9e8ef6d2f0069715fccf81
Saalim Zafar <saleem.zafar9910@gmail.com>
2022-11-14 11:52:54 +0530

    Create timer.js

---
+++ pages/timer.js
@@ -0,0 +1,9 @@
+import TimersDashboard from '../components/TimersDashboard.js'
+export default function Index() {
+return (
+<>
+<h1 className='ui dividing centered header'>Timers</h1>
+<TimersDashboard />
+</>
+)
+}
