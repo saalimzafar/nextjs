fee718b4600e0e452a19a6d6980f1022932c544a
Saalim Zafar <saleem.zafar9910@gmail.com>
2023-01-02 19:26:16 +0530

    Update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import fs from 'fs'
 export default (req, res) => {
+  let data = "Hello World"
+  fs.writeFile("test.json", data, err => {})
   res.statusCode = 200
   res.json({ name: 'Saalim Zafar' })
+  
 }
