4d20c103bb407c9e24d8e15b3460b36d5fbd6e2c
Saalim Zafar <saleem.zafar9910@gmail.com>
2023-01-02 19:23:01 +0530

    Update

--- pages/api/file.js
+++ pages/api/file.js
@@ -1,6 +1,6 @@
 
 import fs from 'fs'
-export default handler(req,res) {
+export default handler = (req,res) => {
 let data = "Hello World"
 fs.writeFile('test.json', data, err => {})
 res.json({"name":"Saalim"})
