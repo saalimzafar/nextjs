f991226edb9c71f30e354dc0cddfdf5a418484ea
Saalim Zafar <saleem.zafar9910@gmail.com>
2023-01-02 18:30:02 +0530

    Update

--- pages/api/pet/create.js
+++ pages/api/pet/create.js
@@ -1,9 +1,9 @@
 import fs from "fs";
 export default (req, res) => {
-  let petData = require("../../../petData.json");
+  
   let formData = req.body;
-  petData.push(formData);
-  let data = JSON.stringify(petData);
+  
+  let data = JSON.stringify(formData);
   const writeData = () => {
     fs.writeFile("petData.json", data, (err) => {
       if (err) throw err;
